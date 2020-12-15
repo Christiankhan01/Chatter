5f00de24433e240639f5759df01ce9c36db19b3e
Christian Khan <christiankhan@Hotmail.com>
2020-12-15 18:12:06 -0500

    state hook added

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,8 +1,22 @@
-import React from 'react'; 
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Join = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
     return (
-        <h1>Join</h1>
+        <div className="joinOuter">
+            <div className="joinInner">
+                <h1 className="heading">Join</h1>
+                <div><input placeholder="Name" className="joinInput" type="text" onChange={(e) => setName(e.target.value) }  /></div>
+                <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={(e) => setRoom(e.target.value)} /></div>
+                <Link>
+                    <button className="button mt-20" type="submit">Sign In</button>
+                </Link>
+                <div></div>
+            </div>
+        </div>
     )
 }
 
