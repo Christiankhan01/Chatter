8ee057374fb39dc24505d2c1cf4bdb2c989f3685
Christian Khan <christiankhan@Hotmail.com>
2020-12-27 18:48:39 -0500

    user.js and msg emit added

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -20,10 +20,14 @@ const Chat = ({ location }) => {
         
         console.log(socket); 
         
-        socket.emit('join', {name, room}, ({error}) => {
-            alert(error); 
-        }); 
+        socket.emit('join', {name, room}, () => {
 
+        }); 
+        return () => {
+            socket.emit('disconnect'); 
+            
+            socket.off(); 
+        }
     }, [ENDPOINT, location.search]); 
     return (
         <h1>Chat</h1>
